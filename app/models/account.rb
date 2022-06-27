879917d80a592221a4baa964ecd16cffe504e361
Benjamin Liet <liet.ben@gmail.com>
2022-06-27 15:04:18 +0200

    Add associations and validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ApplicationRecord
+  has_many :account_documents
+  has_many :documents, through: :account_documents
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_accounts
+  has_many :order_documents
+
+  validates :deceased_first_name, :deceased_last_name,
+            presence: true,
+            format: { with: /\A\D+\z/, message: "ne doit contenir que des lettres" }
 end

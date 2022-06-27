9865060ff0b0049582594a01d59d41b2bb329013
Benjamin Liet <liet.ben@gmail.com>
2022-06-27 15:53:12 +0200

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :orders do
+    member do
+      get 'recap'
+    end
+  end
+
+  resources :orders, except: %i[index show] do
+    resources :order_accounts, only: %i[new create destroy]
+    resources :order_accounts, except: %i[index destroy]
+  end
+
+  resources :users, only: :show do
+    resources :orders, only: :show
+  end
 end

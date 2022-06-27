4c711dfd47dd94a81b7c11d8b194dd513a483be3
Marc Delesalle <mrc.delesalle@gmail.com>
2022-06-27 17:03:55 +0200

    first orders_controller methods and btn on home

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,20 @@
 class OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    return unless @order.save
+
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(%i[deceased_first_name deceased_last_name])
+  end
 end

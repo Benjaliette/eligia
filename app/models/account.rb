44d6b6173c072952f1d5f06c7257506e567285d8
Benjamin Liet <liet.ben@gmail.com>
2022-06-29 17:07:06 +0200

    Delete account if not created in new order form

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,6 @@ class Account < ApplicationRecord
   has_many :documents, through: :account_documents
   has_many :order_accounts, dependent: :destroy
   belongs_to :category
+
+  validates :name, presence: true
 end
--- app/models/order_account.rb
+++ app/models/order_account.rb
@@ -2,5 +2,9 @@ class OrderAccount < ApplicationRecord
   belongs_to :order
   belongs_to :account
 
-  accepts_nested_attributes_for :account, allow_destroy: true
+  accepts_nested_attributes_for :account, allow_destroy: true, reject_if: :reject_accounts
+
+  def reject_accounts(attributes)
+    attributes['name'].blank?
+  end
 end

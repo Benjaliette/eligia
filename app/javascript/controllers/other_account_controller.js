b91e9243d6fa02af7c85ecc5a42ae77792cb48e5
Marc Delesalle <mrc.delesalle@gmail.com>
2022-06-29 14:18:39 +0200

    js active remove when click

--- app/javascript/controllers/other_account_controller.js
+++ app/javascript/controllers/other_account_controller.js
@@ -2,13 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="other-account"
 export default class extends Controller {
+  static targets = ['radioButtons', "otherButton"]
   connect() {
     console.log('connected')
   }
 
-  clicked(event) {
-    console.log('benjadev')
-    console.log(event.target)
+  otherclicked(event) {
+
+    console.log('other clicked function')
+
+    this.radioButtonsTargets.forEach(radioButton => {
+      console.log(radioButton)
+      radioButton.checked = false
+    });
+
     event.target.classList.toggle('active')
+  }
+
+  radioclicked(event) {
+
+    console.log('radio clicked function')
+    this.otherButtonTarget.classList.remove('active')
   }
 }

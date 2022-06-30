67eee0b1a39c1a7b2590b800b1eb17f9251839a4
Benjamin Liet <liet.ben@gmail.com>
2022-06-30 11:43:54 +0200

    Add to db the packs table + association with order + created columns for paiement with stripe in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
+  monetize :amount_cents
+
   belongs_to :user
   has_many :order_accounts
   has_many :order_documents
+  has_one :pack
 
   validates :deceased_first_name, :deceased_last_name,
             presence: true,

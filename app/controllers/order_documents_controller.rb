3ec78f5390c8ecba6fc4ccde8cc250adfef6f321
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-01 17:54:27 +0200

    still not working

--- app/controllers/order_documents_controller.rb
+++ app/controllers/order_documents_controller.rb
@@ -2,14 +2,14 @@ class OrderDocumentsController < ApplicationController
   def create
     @order_document = OrderDocument.new(order_document_params)
     @order_document.save
-    redirect_to add_documents_order_path(Order.find(params[:order_id]))
+    # redirect_to add_documents_order_path(@order_document.order)
   end
 
   def update
     @order_document = OrderDocument.find(params[:id])
     @order_document.update(order_document_params)
     @order_document.save
-    redirect_to add_documents_order_path(Order.find(params[:order_id]))
+    redirect_to add_documents_order_path(@order_document.order)
   end
 
   private

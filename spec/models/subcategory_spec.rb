834f76b2c91435aa088029206040991c9af70dc7
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-06 15:45:04 +0200

    spec for Subcategory

--- spec/models/subcategory_spec.rb
+++ spec/models/subcategory_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Subcategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Creating a subcategory" do
+
+    it "Should be valid when it has a name and an existing category" do
+      subcategory = build(:subcategory)
+      expect(subcategory.valid?).to eq true
+    end
+
+    it "Should have a unique name" do
+      create(:subcategory)
+      subcategory = Subcategory.new(name: 'subcateforyName')
+      expect(subcategory.valid?).to eq false
+    end
+  end
 end

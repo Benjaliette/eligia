82916a58fb2e1b5b00e8d0dc3ac0e2d345b8cea3
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-06 18:03:40 +0200

    spec for order --> packs not working

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,68 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  let(:user) { User.new(first_name: 'Benjamin', last_name: 'Liet', email: 'bl@eligia.fr', password: '123456') }
-  let(:pack) { Pack.new(title: 'Pack', price: 100, level: 2) }
-  let(:order) { Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: user, pack: pack) }
 
   context "When creating an order" do
-    it "Should be valid when everything is specified" do
+    it "Should be valid with a deceased_first/last_name and user" do
+      order = build(:order)
       expect(order.valid?).to eq true
     end
 
-    # Pack
-    it "Should have a pack" do
-      order = Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: user)
+    it "Should be invalid w/o a deceased_first_name" do
+      order = build(:order, deceased_first_name: "")
       expect(order.valid?).to eq false
     end
 
-    it "Should have a valid pack_id" do
-      order = Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: user, pack_id: 0)
+    it "Should be invalid w/o a deceased_last_name" do
+      order = build(:order, deceased_last_name: "")
       expect(order.valid?).to eq false
     end
 
-    # User
-    it "Should have a user" do
-      order = Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', pack: pack)
+    it "Should be invalid if deceased_first_name does not match regex" do
+      order = build(:order, deceased_first_name: "m4rc")
       expect(order.valid?).to eq false
     end
 
-    it "Should have a valid user_id" do
-      order = Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user_id: 0, pack: pack)
+    it "Should be invalid if deceased_first_name does not match regex" do
+      order = build(:order, deceased_last_name: "doe!")
       expect(order.valid?).to eq false
     end
   end
 
-  describe "#determine_pack_type" do
-    Category.create(name: 'Telecom')
-    Subcategory.create(name: 'Ligne mobile', category: Category.find_by(name: 'Telecom'))
-    Subcategory.create(name: 'Ligne fixe', category: Category.find_by(name: 'Telecom'))
-    Account.create(name: 'Bouygues', subcategory: Subcategory.find_by(name: 'Ligne mobile'), status: 'validated')
-    Account.create(name: 'Free', subcategory: Subcategory.find_by(name: 'Ligne fixe'), status: 'validated')
-    my_pack = Pack.create(title: 'Pack', price: 100, level: 1)
-    let(:order_account) { OrderAccount.create(order: order, account: Account.find_by(name: 'Bouygues')) }
+  # describe "#determine_pack_type" do
 
-    it "Assigns the appropriate pack to an order" do
-      expect(order.determine_pack_type).to eq my_pack
-    end
+  #   it "Should assign pack #1 for 4 OrderAccounts" do
+  #     create_list(:pack, 4)
+  #     default_pack = create(:pack, title: 'Default_pack', price: 2000)
+  #     order = create(:order, pack: default_pack)
+  #     build_stubbed_list(:order_account, 4, order: order)
+  #     expect(order.determine_pack_type).to eq Pack.find_by(title: 'packTitle1')
+  #   end
 
-    it "Should not assign an unappropriate pack to an order" do
-      expect(order.determine_pack_type).not_to eq Pack.where(level: 2).last
-    end
-  end
+  #   it "Should assign pack #2 for 10 OrderAccounts" do
+  #     create_list(:pack, 4)
+  #     default_pack = create(:pack, title: 'Default_pack', price: 2000)
+  #     order = create(:order, pack: default_pack)
+  #     build_stubbed_list(:order_account, 10, order: order)
+  #     expect(order.determine_pack_type).to eq Pack.find_by(title: 'packTitle2')
+  #   end
 
-  describe "#required_documents" do
-    User.create(first_name: 'Benjamin', last_name: 'Liet', email: 'bl@eligia.fr', password: '123456')
-    Pack.create(title: 'Pack', price: 100, level: 2)
-    order = Order.create(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: User.last, pack: Pack.last)
-    Document.create(name: 'Certificat décès', format: 'pdf')
-    Document.create(name: 'Pièce identité', format: 'pdf')
-    AccountDocument.create(account: Account.find_by(name: 'Free'), document: Document.find_by(name: 'Certificat décès'))
-    AccountDocument.create(account: Account.find_by(name: 'Free'), document: Document.find_by(name: 'Pièce identité'))
-    OrderAccount.create(order: order, account: Account.find_by(name: 'Free'))
-
-    it "Has the right required documents" do
-      expect(order.required_documents.sort).to match_array [Document.find_by(name: 'Certificat décès'), Document.find_by(name: 'Pièce identité')].sort
-    end
+  #   it "Should assign pack #3 for 11 OrderAccounts" do
+  #     create_list(:pack, 4)
+  #     default_pack = build_stubbed(:pack, title: 'Default_pack', price: 2000)
+  #     order = build_stubbed(:order, pack: default_pack)
+  #     build_stubbed_list(:order_account, 11, order: order)
+  #     expect(order.determine_pack_type).to eq Pack.find_by(title: 'packTitle2')
+  #   end
   end
 end

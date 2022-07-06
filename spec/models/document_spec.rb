26c97550319026dd9199e800fd722390039cdbd7
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-06 16:30:20 +0200

    spec for documents and accounts

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "When creating a document" do
+
+    it "Should be valid with a name and a format" do
+      document = build(:document)
+      expect(document.valid?).to eq true
+    end
+
+    it "Should be invalid w/o a name" do
+      document = build(:document, name: "")
+      expect(document.valid?).to eq false
+    end
+
+    it "Should be invalid w/o a format" do
+      document = build(:document, format: "")
+      expect(document.valid?).to eq false
+    end
+
+    it "Should be invalid if the name is already taken" do
+      create(:document, name: "id")
+      document = Document.new(name: "id", format: "pdf")
+      expect(document.valid?).to eq false
+    end
+
+  end
 end

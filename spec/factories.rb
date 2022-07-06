e2cfd1f50322e7315e1d7120659689ca55fd0570
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-06 15:38:08 +0200

    specs for category

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
+  # User
   factory :user do
     first_name { "John" }
     last_name  { "Doe" }
     email { "#{first_name}.#{last_name}@eligia.fr".downcase }
     admin { false }
     password { "123456" }
+  end
+
+  # Category
+  factory :category do
+    name { "CategoryName" }
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Creating categories" do
+
+    it "Should have a name" do
+      category = build_stubbed(:category, name: 'Telecom')
+      expect(category.valid?).to eq true
+    end
+
+    it "Should have a unique name" do
+      create(:category, name: 'Telecom')
+      category2 = Category.new(name: 'Telecom')
+      expect(category2.valid?).to eq false
+    end
+  end
 end

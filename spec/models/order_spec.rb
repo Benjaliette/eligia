5c6a01212a658b403d6e54cd8663ed6c81097c8d
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-06 12:47:01 +0200

    order required documents test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   let(:user) { User.new(first_name: 'Benjamin', last_name: 'Liet', email: 'bl@eligia.fr', password: '123456') }
-  let(:pack) { Pack.new(title: 'Pack', price_cents: 1000) }
+  let(:pack) { Pack.new(title: 'Pack', price: 100, level: 2) }
   let(:order) { Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: user, pack: pack) }
 
   context "When creating an order" do
@@ -52,12 +52,14 @@ RSpec.describe Order, type: :model do
   end
 
   describe "#required_documents" do
-    let(:order) { Order.new(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: user, pack: pack) }
+    User.create(first_name: 'Benjamin', last_name: 'Liet', email: 'bl@eligia.fr', password: '123456')
+    Pack.create(title: 'Pack', price: 100, level: 2)
+    order = Order.create(deceased_first_name: 'Marc', deceased_last_name: 'Delesalle', user: User.last, pack: Pack.last)
     Document.create(name: 'Certificat décès', format: 'pdf')
     Document.create(name: 'Pièce identité', format: 'pdf')
     AccountDocument.create(account: Account.find_by(name: 'Free'), document: Document.find_by(name: 'Certificat décès'))
     AccountDocument.create(account: Account.find_by(name: 'Free'), document: Document.find_by(name: 'Pièce identité'))
-    let(:order_account) { OrderAccount.new(order: order, account: Account.find_by(name: 'Free')) }
+    OrderAccount.create(order: order, account: Account.find_by(name: 'Free'))
 
     it "Has the right required documents" do
       expect(order.required_documents.sort).to match_array [Document.find_by(name: 'Certificat décès'), Document.find_by(name: 'Pièce identité')].sort

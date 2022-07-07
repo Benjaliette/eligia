45a6190727d8b81c02924f9b7420c76905d3c57b
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-07 11:59:11 +0200

    debut creation des tests systems

--- spec/models/order_document_spec.rb
+++ spec/models/order_document_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe OrderDocument, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "When creating an order_document" do
+
+    it "Should be valid with an order and an document" do
+      order_document = build(:order_document)
+      expect(order_document.valid?).to eq true
+    end
+
+    it "Should not be valid w/o an order" do
+      order_document = build(:order_document, order: nil)
+      expect(order_document.valid?).to eq false
+    end
+
+    it "Should not be valid w/o an document" do
+      order_document = build(:order_document, document: nil)
+      expect(order_document.valid?).to eq false
+    end
+
+  end
 end
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "pages", type: :request do
+  describe "pages#home" do
+    get '/'
+
+  end
+
+  describe "pages#price" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+
+  describe "pages#contact" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end

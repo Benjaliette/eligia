5fdb44b0847f76423be97ca8d18ab7791b356c76
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-07 12:40:00 +0200

    first system tests, no warden yet

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,7 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "pages", type: :system do
-  describe "pages#home" do
-    get '/'
+  context "pages#home" do
+
+    # See pages#home
+    it "Can find catch phrase" do
+      visit "/"
+      expect(page).to have_text("Résiliez en 10 minutes tous les\nabonnements d'un proche disparu")
+    end
+
+    # Access pages#price
+    it "Has a link towarts /pages/price" do
+      visit "/"
+      expect(page).to have_link(nil, href: '/pages/price')
+    end
+
+    it "Can click the link towarts /pages/price and see the page title" do
+      visit "/"
+      click_link(nil, href: '/pages/price')
+      expect(page).to have_text("Page des prix")
+    end
+
+    # Access pages#contact
+    it "Has a link towarts /pages/contact" do
+      visit "/"
+      expect(page).to have_link(nil, href: '/pages/contact')
+    end
+
+    it "Can click the link towarts /pages/price and see the page title" do
+      visit "/"
+      click_link(nil, href: '/pages/contact')
+      expect(page).to have_text("Page de contact")
+    end
+
+    # Access orders#new
+    it "Has a link towarts /orders/new" do
+      visit "/"
+      expect(page).to have_link(nil, href: '/pages/contact')
+    end
+
+    it "Can click the link towarts /orders/new" do
+      visit "/"
+      click_link(nil, href: '/orders/new')
+      expect(page).to have_text("S'identifier")
+    end
   end
 end

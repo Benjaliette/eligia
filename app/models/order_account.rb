be1c95ca56a910530a9a5ad6d00b9a0a4fddda19
Marc Delesalle <mrc.delesalle@gmail.com>
2022-07-11 11:22:31 +0200

    Add after_commit_everywhere + declare first states of order_accounts

--- app/models/order_account.rb
+++ app/models/order_account.rb
@@ -1,4 +1,6 @@
 class OrderAccount < ApplicationRecord
+  include AASM
+
   belongs_to :order
   belongs_to :account
   has_many :account_documents
@@ -7,5 +9,30 @@ class OrderAccount < ApplicationRecord
 
   def reject_accounts(attributes)
     attributes['name'].blank?
+  end
+
+  # State Machine
+  aasm do
+    state :documents_missing, :pending, :sent, :failed, :succeded
+
+    event :declare_missing do
+      transitions from: :any, to: :documents_missing
+    end
+
+    event :declare_pending do
+      transitions from: :any, to: :pending
+    end
+
+    event :send do
+      transitions from: :pending, to: :sent
+    end
+
+    event :declare_success do
+      transitions from: :sent, to: :succeded
+    end
+
+    event :declare_failure do
+      transitions from: :any, to: :failed
+    end
   end
 end

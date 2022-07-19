fdd5f56ef3b0aabfc3d1309884b19a7e37911e61
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-07-19 17:33:47 +0200

    Starting to change home infos div to horizontal timeline to click

--- app/javascript/controllers/home_infos_appear_controller.js
+++ app/javascript/controllers/home_infos_appear_controller.js
@@ -5,22 +5,5 @@ export default class extends Controller {
   static targets = [ "infoItems" ]
 
   transition() {
-    this.infoItemsTargets.forEach((infoItem, i) => {
-      const divTop = infoItem.getBoundingClientRect().top
-
-      if (divTop < 400) {
-        infoItem.classList.add('active')
-      }
-
-      if (i === 0 && window.scrollY > 2000) {
-        infoItem.classList.remove('active')
-      } else if (i === 1 && window.scrollY > 3000) {
-        infoItem.classList.remove('active')
-      } else if (i === 2 && window.scrollY > 3800) {
-        infoItem.classList.remove('active')
-      }
-
-    });
-
   }
 }

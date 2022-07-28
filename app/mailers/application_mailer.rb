24c7721eaa58f7e3f8eb401c3cfcc4e48d1ac880
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-07-28 11:24:45 +0200

    attempt to fix absent pic in mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  before_action :add_attachment
-
   default from: "contact@eligia.fr"
   layout "mailer"
 
-  private
-
-  def add_attachment
-    attachments.inline['eligia.svg'] = File.read('app/assets/images/eligia-sans-fond-vecto.svg')
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = params[:user]
-    attachments.inline['eligia.svg'] = File.read('app/assets/images/eligia-sans-fond-vecto.svg')
 
     mail to: @user.email, subject: "Confirmation de votre inscription à Eligia"
   end

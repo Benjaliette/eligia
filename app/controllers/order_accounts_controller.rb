a34db704c060594235c9e390a0eb9e54c036d34f
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-15 16:04:04 +0200

    remove format .pdf for order_account show -> Because there won't be any other show anyways and it enables rails admin to display the resiliation pdf, which will be handy at first before automation

--- app/controllers/order_accounts_controller.rb
+++ app/controllers/order_accounts_controller.rb
@@ -2,17 +2,12 @@ class OrderAccountsController < ApplicationController
   before_action :set_order_account, only: %i[show]
 
   def show
-    respond_to do |format|
-      format.html { render :show }
-      format.pdf do
-        pdf = OrderAccountPdf.new(@order_account)
-        pdf.resiliation_pdf
-        send_data pdf.render,
-                  filename: "export.pdf",
-                  type: 'application/pdf',
-                  disposition: 'inline'
-      end
-    end
+    pdf = OrderAccountPdf.new(@order_account)
+    pdf.resiliation_pdf
+    send_data pdf.render,
+              filename: "export.pdf",
+              type: 'application/pdf',
+              disposition: 'inline'
   end
 
   private

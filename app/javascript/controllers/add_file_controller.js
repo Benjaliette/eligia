e3a750f05a0fff9714cc6cdb05326f6b13f52a00
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-17 16:58:17 +0200

    js validation pour le format des documents

--- app/javascript/controllers/add_file_controller.js
+++ app/javascript/controllers/add_file_controller.js
@@ -5,8 +5,18 @@ export default class extends Controller {
   static targets = ['input', 'label', 'fileNameDiv']
 
   added(){
-    this.fileNameDivTarget.innerText = this.inputTarget.files[0].name
-    this.labelTarget.classList.add('label-document-input-selected')
-    this.labelTarget.innerText = 'Modifier'
+    const regex = /.*.(?:png|jpg)/
+
+    if (this.inputTarget.files[0].name.match(regex) === null) {
+      this.fileNameDivTarget.innerText = 'Le format doit être .png ou .jpg'
+      this.labelTarget.classList.add('label-document-input-wrong-format')
+      this.labelTarget.innerText = 'ERREUR'
+
+    }
+    else {
+      this.fileNameDivTarget.innerText = this.inputTarget.files[0].name
+      this.labelTarget.classList.add('label-document-input-selected')
+      this.labelTarget.innerText = 'Modifier'
+    }
   }
 }

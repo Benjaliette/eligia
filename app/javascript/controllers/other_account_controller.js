c1fc4afe9af3b9555fff297b44835ab9c5b651ad
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-08-19 11:22:25 +0200

    change radio buttons to checkboxes

--- app/javascript/controllers/other_account_controller.js
+++ app/javascript/controllers/other_account_controller.js
@@ -5,7 +5,7 @@ let lastclicked;
 
 // Connects to data-controller="other-account"
 export default class extends Controller {
-  static targets = ['radioButtons', "otherButton", "subcategoryDiv"]
+  static targets = ['checkBoxes', "otherButton", "subcategoryDiv"]
 
   static values = {
     order: Object,
@@ -14,58 +14,55 @@ export default class extends Controller {
   }
 
   connect() {
-    this.radioButtonsTargets.forEach((account) => {
-      this.orderValue.accounts.forEach((orderAccount) => {
-        if (account.value == orderAccount.account_id) {
-          account.setAttribute('checked', true)
-        } else if (orderAccount.account_id >= Math.floor(this.accountNumberValue) + 1) {
-          if (this.subcategoryValue == orderAccount.account_subcategory) {
-            this.otherButtonTarget.classList.add('active')
-            this.otherButtonTarget.innerText = orderAccount.account_name.replace('_', ' ')
-          }
-        }
-      })
-    })
+    try {
+      this.checkBoxesTargets.forEach((account) => {
+        this.orderValue.accounts.forEach((orderAccount) => {
+          if (account.value == orderAccount.account_id) {
+            account.setAttribute('checked', true);
+          } else if (orderAccount.account_id >= Math.floor(this.accountNumberValue) + 1) {
+            if (this.subcategoryValue == orderAccount.account_subcategory) {
+              this.otherButtonTarget.classList.add('active');
+              this.otherButtonTarget.innerText = orderAccount.account_name.replace('_', ' ');
+            };
+          };
+        });
+      });
+    }
+    catch(err) {
+    }
+
   }
 
   otherclicked(event) {
-    this.radioButtonsTargets.forEach(radioButton => {
-      radioButton.checked = false
-    });
-
     this.subcategoryDivTarget.classList.toggle('display-none');
-    console.log(this.otherButtonTarget.innerText)
-    if (this.otherButtonTarget.innerText == 'Autre' || !this.otherButtonTarget.classList.contains('active')) {
-      this.otherButtonTarget.classList.toggle('active');
-    }
-
-    lastclicked = event.target;
+    this.otherButtonTarget.classList.add('active');
   }
 
-  radioclicked(event) {
-    radioCheckNumber ++
-
-    this.subcategoryDivTarget.classList.add('display-none');
-    this.otherButtonTarget.classList.remove('active');
-
-    if (radioCheckNumber % 2 === 0 && lastclicked === event.target) {
-      event.target.checked = false
-    } else {
-      event.target.checked = true
-    }
-
-    lastclicked = event.target;
-  }
 
   buttonText(event) {
-    if (event.key.length === 1) {
-      this.otherButtonTarget.textContent = event.srcElement.value;
-    }
+    if (event.key.length === 1 || event.keyCode === 8) {
+      if (event.srcElement.value == '') {
+        this.otherButtonTarget.textContent = 'Autre';
+      } else {
+        this.otherButtonTarget.textContent = event.srcElement.value;
+      };
+
+    };
   }
 
   preventSubmit(event) {
     if(event.keyCode === 13) {
       event.preventDefault()
-    }
+    };
+  }
+
+  accountTyped() {
+    this.subcategoryDivTarget.classList.add('display-none');
+  }
+
+  removeAccount() {
+    this.subcategoryDivTarget.classList.add('display-none');
+    this.otherButtonTarget.textContent = 'Autre';
+    this.otherButtonTarget.classList.remove('active');
   }
 }

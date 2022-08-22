af4cf3693908e10df97e30e6513c6de9f58b10c2
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-22 18:58:10 +0200

    changer commande en demande et ajout de Eligia au début des objects des mailers

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -3,11 +3,11 @@ class DeviseMailer < Devise::Mailer
 
   def confirmation_instructions(record, opts={})
     @mail = super
-    mail to: record.email, subject: "Nous avons besoin de votre confirmation"
+    mail to: record.email, subject: "Eligia - Nous avons besoin de votre confirmation"
   end
 
   def reset_password_instructions(record, token, opts={})
     @mail = super
-    mail to: record.email, subject: "Changement de votre mot de passe"
+    mail to: record.email, subject: "Eligia - Changement de votre mot de passe"
   end
 end

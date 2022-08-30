aa35591893e17da865ffdbfe62908d78ff086f9a
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-08-30 18:58:04 +0200

    if not connect when order recap, go to sign_up then redirect to recap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -21,5 +22,14 @@ class ApplicationController < ActionController::Base
 
   def after_update_path_for(resource)
     user_path(resource)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end

3f39d09c8f68c074d1819d06383afe20e5cc4ee5
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-30 17:43:50 +0200

    status: :unprocessable_entity + french error messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,8 @@ class Message
   include ActiveModel::Model
   attr_accessor :name, :email, :body
 
-  validates :name, :body, presence: true
+  validates :name, :body, presence: { message: "Ne pas laisser vide" }
   validates :email,
-    presence: true,
-    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "doit être une adresse mail compatible" }
+    presence: { message: "Ne pas laisser vide" },
+    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "doit être une adresse mail compatible", message: "Adresse email invalide" }
 end

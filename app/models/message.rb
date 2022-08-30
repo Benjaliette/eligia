6cc97c9a85dcf032934e685f8f933f93a9760adb
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-30 18:12:19 +0200

    correction

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,5 +5,5 @@ class Message
   validates :name, :body, presence: { message: "Ne pas laisser vide" }
   validates :email,
     presence: { message: "Ne pas laisser vide" },
-    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "doit être une adresse mail compatible", message: "Adresse email invalide" }
+    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Adresse email invalide" }
 end

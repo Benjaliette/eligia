0abc5d2fe931e9f2671f502b1c5c013eb59eb4e3
Marc Delesalle <mrc.delesalle@gmail.com>
2022-08-30 17:43:50 +0200

    status: :unprocessable_entity + french error messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,14 +13,14 @@ class MessagesController < ApplicationController
       redirect_to new_message_url
       flash[:success] = "Nous avons bien reçu votre message et nous vous répondrons le plus rapidement possible"
     else
+      render :new, status: :unprocessable_entity
       flash[:alert] = "Il y a eu un problème dans votre demande de contact. S'il vous plait, réessayez."
-      render :new
     end
   end
 
   private
 
   def message_params
-      params.require(:message).permit(:name, :email, :body)
+    params.require(:message).permit(:name, :email, :body)
   end
 end

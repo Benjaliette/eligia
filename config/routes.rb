35410b92eb34c7d9fb2519979ee2a858a895e66c
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-09-01 15:00:48 +0200

    change url everywhere to be more specific and frenchy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users, path: 'utilisateur', controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords: 'users/passwords'
+  }, path_names: {
+    sign_in: 'connexion',
+    sign_up: 'inscription',
+    edit: 'modification'
   }
 
   root to: "pages#home"
 
-  resources :orders, except: %i[index] do
+  resources :orders, path: 'resiliations', path_names: { new: 'contrats', edit: 'documents' }, except: %i[index] do
     member do
-      get 'change'
+      get :change, path: 'contrats'
       patch 'update_documents'
-      get 'recap'
+      get :recap, path: 'recapitulatif'
       get 'success'
       get 'paiement'
     end
@@ -20,25 +24,25 @@ Rails.application.routes.draw do
   # Redirect to orders/new if there is a refresh after a render :new
   get '/orders/', to: 'orders#new'
 
-  resources :users, only: %i[show index edit update] do
+  resources :users, path: 'utilisateur', path_names: { edit: 'modification' }, only: %i[show index edit update] do
     member do
-      get 'control_password'
+      get :control_password, path: 'controle'
       patch 'control_password_check'
     end
   end
 
-  resources :order_accounts, only: :show do
-    resources :order_documents, only: %i[index update]
+  resources :order_accounts, path: 'contrats', only: :show do
+    resources :order_documents, path: 'documents', only: %i[index update]
   end
   resources :order_documents, only: :create
 
   resources :pages do
     collection do
-      get 'price'
+      get :price, 'tarifs'
     end
   end
 
-  resources :messages, only: %i[new create], path: 'contact'
+  resources :messages, path: 'contact', path_names: { new: 'nous-contacter' }, only: %i[new create]
 
   mount RailsAdmin::Engine, at: '/admin', as: 'rails_admin'
   mount StripeEvent::Engine, at: '/paiement-success'

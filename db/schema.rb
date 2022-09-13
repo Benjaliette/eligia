3dea22aa99b05c829fcb64505f2e0501c9a0189c
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-09-13 18:21:14 +0200

    redo entirely user dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_181621) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_122848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_181621) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "content"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_notifications_on_order_id"
+  end
+
+  create_table "order_account_order_documents", force: :cascade do |t|
+    t.bigint "order_account_id", null: false
+    t.bigint "order_document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_account_id"], name: "index_order_account_order_documents_on_order_account_id"
+    t.index ["order_document_id"], name: "index_order_account_order_documents_on_order_document_id"
   end
 
   create_table "order_accounts", force: :cascade do |t|
@@ -151,6 +168,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_181621) do
   add_foreign_key "accounts", "subcategories"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "notifications", "orders"
+  add_foreign_key "order_account_order_documents", "order_accounts"
+  add_foreign_key "order_account_order_documents", "order_documents"
   add_foreign_key "order_accounts", "accounts"
   add_foreign_key "order_accounts", "orders"
   add_foreign_key "order_documents", "documents"

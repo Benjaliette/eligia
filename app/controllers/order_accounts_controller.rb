4820d099d2e0d92ea450c23d50db85003806e241
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-09-27 11:25:07 +0200

    add some front to account cards in order new

--- app/controllers/order_accounts_controller.rb
+++ app/controllers/order_accounts_controller.rb
@@ -1,4 +1,5 @@
 class OrderAccountsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[create]
   before_action :set_order_account, only: %i[show edit update]
   before_action :set_order, only: %i[show]
   after_action :set_pundit_order_account, only: %i[show create]
@@ -11,11 +12,11 @@ class OrderAccountsController < ApplicationController
   end
 
   def create
-    @order_account = OrderAccount.create(order_account_params)
+    @order_account = OrderAccount.new(order_account_params)
 
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.append('test', partial: "order_accounts/account_card",
+        render turbo_stream: turbo_stream.append('account-list', partial: "order_accounts/account_card",
           locals: { order_account: @order_account })
       end
       format.html { redirect_to new_order_path, notice: "Le compte a bien été ajouté à la liste" }

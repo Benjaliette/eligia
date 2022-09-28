356cfa57bccc352ca3aa8ab97e2ff5127c46273b
Marc Delesalle <mrc.delesalle@gmail.com>
2022-09-28 15:42:39 +0200

    s'more

--- app/javascript/controllers/scroll_navbar_controller.js
+++ app/javascript/controllers/scroll_navbar_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = [ 'navbar', 'button' ]
 
   connect() {
-    // this.changeBackground()
+    this.changeBackground()
   }
 
   changeBackground() {

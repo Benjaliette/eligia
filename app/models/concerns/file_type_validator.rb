82d68d765afb54117caedb39f9f7aa1f856fc871
Marc <101124539+M-a-rc@users.noreply.github.com>
2022-10-03 14:15:22 +0200

    Update file_type_validator.rb

--- app/models/concerns/file_type_validator.rb
+++ app/models/concerns/file_type_validator.rb
@@ -3,22 +3,22 @@ class FileTypeValidator < ActiveModel::EachValidator
     unless record.document.nil? || value.nil?
       case record.document.file_type
       when 'iban'
-        record.errors.add attribute, (options[:message] || "ne correspond pas au format IBAN") unless
+        record.errors.add attribute, (options[:message] || "IBAN incorrect") unless
         value.gsub(' ', '') =~ /^FR\d{12}[A-Z0-9]{11}\d{2}$/i || value.empty?
       when 'compteur_gaz'
-        # record.errors.add attribute, (options[:message] || "n'est pas un IBAN") unless
+        # record.errors.add attribute, (options[:message] || "Relevé incorrect") unless
         # value =~ /^FR\d{12}[A-Z0-9]{11}\d{2}$/i
       when 'compteur_elec'
-        # record.errors.add attribute, (options[:message] || "n'est pas un IBAN") unless
+        # record.errors.add attribute, (options[:message] || "Relevé incorrect") unless
         # value =~ /^FR\d{12}[A-Z0-9]{11}\d{2}$/i
       when 'compteur_eau'
-        # record.errors.add attribute, (options[:message] || "n'est pas un relevé de compteur") unless
+        # record.errors.add attribute, (options[:message] || "Relevé incorrect") unless
         # value =~ /^FR\d{12}[A-Z0-9]{11}\d{2}$/i || value.empty?
       when 'telephone_mobile'
-        record.errors.add attribute, (options[:message] || "n'est pas un numéro de téléphone correct") unless
+        record.errors.add attribute, (options[:message] || "Numéro incorrect") unless
         value =~ /(\(\+33\)|0|\+33|0033)[1-9]([0-9]{8}|([0-9\- ]){12})/ || value.empty?
       when 'telephone_fixe'
-        record.errors.add attribute, (options[:message] || "n'est pas un numéro de téléphone correct") unless
+        record.errors.add attribute, (options[:message] || "Numéro incorrect") unless
         value =~ /(\(\+33\)|0|\+33|0033)[1-9]([0-9]{8}|([0-9\- ]){12})/ || value.empty?
       end
     end

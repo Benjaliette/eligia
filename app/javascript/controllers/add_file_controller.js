c4c6681bca1871e096ffd75d97f4e4d01c1b9c9c
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-05 18:16:28 +0200

    modal order_documents prettier but f*ing stimulus doesn't work

--- app/javascript/controllers/add_file_controller.js
+++ app/javascript/controllers/add_file_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="add-file"
 export default class extends Controller {
   static targets = ['input', 'label', 'fileNameDiv', 'submitBtn']
+  connect(){
 
+  }
   added(){
     const regex = /.*.(?:png|jpg|jpeg|pdf|JPG|JPEG|PNG|PDF)/
-
     if (this.inputTarget.files[0].size > 1048576 * 10) {
       alert("Fichier trop lourd. Taille maximum 10Mb")
       this.inputTarget.value = ''
@@ -17,6 +18,7 @@ export default class extends Controller {
       this.labelTarget.innerText = 'Modifier'
 
     } else {
+      console.log("✅")
       this.fileNameDivTarget.innerText = ''
       Array.from(this.inputTarget.files).forEach(file => {
         this.fileNameDivTarget.insertAdjacentHTML('beforeend', `<div>${file.name}</div>`)

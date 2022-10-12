2955f64be144b98eee73d102903127ee9bb5edbb
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-12 15:57:39 +0200

    no scroll

--- app/javascript/controllers/scroll_navbar_controller.js
+++ app/javascript/controllers/scroll_navbar_controller.js
@@ -5,21 +5,11 @@ export default class extends Controller {
   static targets = [ 'navbar', 'button' ]
 
   connect() {
+    console.log('hello')
     this.changeBackground()
   }
 
   changeBackground() {
-    if(window.scrollY > 80) {
-      this.navbarTarget.classList.add('white-bg')
-    } else {
-      this.navbarTarget.classList.remove('white-bg')
-    }
-    if(this.hasButtonTarget){
-      if(window.scrollY > 500) {
-        this.buttonTarget.classList.remove('display-none')
-        } else {
-        this.buttonTarget.classList.add('display-none')
-      }
-    }
+
   }
 }

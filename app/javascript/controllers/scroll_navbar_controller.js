382cdc411d1e0d47ac71fa5532a07d3713031dc0
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-12 16:03:31 +0200

    white navbar earlier on mobile

--- app/javascript/controllers/scroll_navbar_controller.js
+++ app/javascript/controllers/scroll_navbar_controller.js
@@ -5,11 +5,27 @@ export default class extends Controller {
   static targets = [ 'navbar', 'button' ]
 
   connect() {
-    console.log('hello')
     this.changeBackground()
   }
 
   changeBackground() {
+    var breakPoint = 80
 
+    if(window.innerWidth < 768){
+      breakPoint = 10
+    }
+
+    if(window.scrollY > breakPoint) {
+      this.navbarTarget.classList.add('white-bg')
+    } else {
+      this.navbarTarget.classList.remove('white-bg')
+    }
+    if(this.hasButtonTarget){
+      if(window.scrollY > 500) {
+        this.buttonTarget.classList.remove('display-none')
+        } else {
+        this.buttonTarget.classList.add('display-none')
+      }
+    }
   }
 }

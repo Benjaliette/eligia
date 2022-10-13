588e573592d6b56fe40c67cdf9a49aa2abeeae37
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-13 10:28:03 +0200

    remove unnecessary

--- app/javascript/controllers/navbar_responsivity_controller.js
+++ app/javascript/controllers/navbar_responsivity_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar-responsivity"
 export default class extends Controller {
-  static targets = [ 'navitems', 'hamburger', 'notifications', 'notificationBtn' ]
+  static targets = [ 'navitems', 'hamburger', 'notifications' ]
   connect() {
     this.toggle()
   }

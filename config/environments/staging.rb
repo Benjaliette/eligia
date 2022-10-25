300a6c02ad8a1cacee6e8845c754081aeceb824d
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-10-25 18:47:28 +0200

    test test

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # Code is not reloaded between requests
   config.cache_classes = true
 
   config.action_controller.asset_host = 'https://www.eligia.fr'

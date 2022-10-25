d958872bcfbb14a249d36e6f0dafba9a978f9d78
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-10-25 18:36:26 +0200

    configure webpack for staging

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path    = require("path")
 const webpack = require("webpack")
 
-module.exports = {
+module.exports = [{
   mode: "production",
   devtool: "source-map",
   entry: {
@@ -17,4 +17,21 @@ module.exports = {
       maxChunks: 1
     })
   ]
-}
+},
+{
+  mode: "staging",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}]

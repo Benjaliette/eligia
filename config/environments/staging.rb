8abdc8f892203b7508ed1cce55cc264f3fa2708c
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-10-26 10:54:15 +0200

    change asset host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_controller.asset_host = 'https://www.eligia.fr'
+  config.action_controller.asset_host = 'https://eligia-preprod-65816.herokuapp.com/'
   config.action_mailer.asset_host = config.action_controller.asset_host
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass

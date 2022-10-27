76607fbc6975d1986dae442ceae8f84f3a154a8d
Marc Delesalle <mrc.delesalle@gmail.com>
2022-10-27 12:21:31 +0200

    m

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -88,15 +88,9 @@ class OrdersController < ApplicationController
     if payment.paid?
       @order = Order.find_by(checkout_session_id: payment.id)
       @order.update(paid: true)
-      p "#✅✅ #{payment.paid?}}"
+      p "#✅✅ #{payment.paid?}"
     else
       p "❌❌#{payment.paid?}"
-      # payment = Mollie::Payment.update(
-      #   params[:id],
-      #   redirect_url: root_url
-      # )
-      redirect_to root_path
-    end
   end
 
   private

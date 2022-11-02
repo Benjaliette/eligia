3e4237904ca36692127836560af951ad8317fc46
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-02 16:34:00 +0100

    add birthdate to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_131729) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_141426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -177,6 +177,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_131729) do
     t.string "phone_number"
     t.bigint "rgpd_id"
     t.boolean "accepted_rgpd", default: false
+    t.date "birthdate"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["rgpd_id"], name: "index_users_on_rgpd_id"

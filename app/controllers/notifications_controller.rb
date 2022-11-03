ce7f9758b95ec72685df3d4bb9718077a0387782
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-03 15:31:23 +0100

    add destruction button for notifications (with modal)

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,14 +1,24 @@
 class NotificationsController < ApplicationController
   before_action :set_notifications, only: :mark_as_read
+  before_action :set_notification, only: :destroy
 
   def mark_as_read
     @notifications.each { |notif| notif.update(read: true) unless notif.read }
     render turbo_stream: turbo_stream.update('notif-icon', partial: 'shared/notification_badge', locals: { notifications: @notifications })
   end
 
+  def destroy
+    @notification.destroy
+    redirect_to user_path(@notification.order.user), status: :see_other
+  end
+
   private
 
   def set_notifications
     @notifications = current_user.notifications
+  end
+
+  def set_notification
+    @notification = Notification.find(params[:id])
   end
 end

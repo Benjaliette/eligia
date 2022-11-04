28e89b63764ea3e038c41d8ca3b4428b367e4ef7
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-04 16:13:00 +0100

    debug

--- app/models/order_account.rb
+++ app/models/order_account.rb
@@ -78,11 +78,15 @@ class OrderAccount < ApplicationRecord
 
   def link_related_files
     storage = Google::Cloud::Storage.new
+    p "❌storage=#{storage}"
     bucket = storage.bucket self.find_bucket, skip_lookup: true
+    p "❌bucket=#{bucket}"
     file_links = []
     bucket.files.each do |file|
       regex = /\A#{self.order.deceased_first_name.gsub(' ', '_')}_#{self.order.deceased_last_name.gsub(' ', '_')}\/#{self.account.name}\/justificatifs\/.+/
+      p "🛑regex=#{regex}"
       if regex.match(file.name)
+        p "✅file_name=#{file.name}"
         file_links << { signed_url: file.signed_url, file_name: file.name.gsub("#{self.order.deceased_first_name.gsub(' ', '_')}_#{self.order.deceased_last_name.gsub(' ', '_')}/#{self.account.name}/justificatifs\/",'') }
       end
     end

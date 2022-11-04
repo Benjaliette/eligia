d2237c95ccbc867c3822207f136db700983d8ba4
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-04 11:55:03 +0100

    made address model test

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,23 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Address, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Address, type: :model do
+  context "When start a payment" do
+    it "Should be valid when entering all values" do
+      order = build(:order)
+      address = build(:address, order: order)
+      expect(order.valid?).to eq true
+    end
+
+    it "Should be invalid w/o a street name" do
+      order = build(:order)
+      address = Address.create(zip: '33000', city: "Bordeaux", state: "France", order: order)
+      expect(order.valid?).to eq false
+    end
+
+    it "Should be invalid when empty" do
+      order = build(:order)
+      address = Address.create(order: order)
+      expect(order.valid?).to eq false
+    end
+  end
+end

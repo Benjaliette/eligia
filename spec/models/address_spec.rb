b4c278d0013b9208dbef509f14a98ddaf70696fa
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-04 15:05:47 +0100

    testParty2 erratum

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,16 +8,9 @@ RSpec.describe Address, type: :model do
       expect(order.valid?).to eq true
     end
 
-    it "Should be invalid w/o a street name" do
-      order = build(:order)
-      address = create(:address, street: nil order:)
-      expect(order.valid?).to eq false
-    end
-
     it "Address errors indexed in order : order invalid if address is invalid" do
       order = build(:order)
-      address =create(:address, street: nil, order: order)
-      expect(order.valid?).to eq false
+      expect { create(:address, street: nil, order:) }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end

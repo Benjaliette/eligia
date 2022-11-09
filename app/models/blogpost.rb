89e2e2de167ffb09091c9dc26994922e43dafbb5
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-09 15:56:04 +0100

    clean blog user journey and add pictures + front

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,6 +1,8 @@
 class Blogpost < ApplicationRecord
+  include ActiveStoragePath
+
   has_rich_text :body
-  has_one_attached :main_picture
+  has_one_attached_with :main_picture, path: -> { "Blog_pictures" }
 
   validates :title, :body, presence: true
 end

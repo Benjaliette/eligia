94a41387f339e02e4ff46bafbc8a7cc3d0348c89
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-10 14:36:55 +0100

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -118,6 +118,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_150236) do
     t.index ["order_id"], name: "index_notifications_on_order_id"
   end
 
+  create_table "order_account_order_documents", force: :cascade do |t|
+    t.bigint "order_account_id", null: false
+    t.bigint "order_document_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_account_id"], name: "index_order_account_order_documents_on_order_account_id"
+    t.index ["order_document_id"], name: "index_order_account_order_documents_on_order_document_id"
+  end
+
   create_table "order_accounts", force: :cascade do |t|
     t.bigint "order_id", null: false
     t.bigint "account_id", null: false
@@ -210,6 +219,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_150236) do
   add_foreign_key "addresses", "orders"
   add_foreign_key "notifications", "order_accounts"
   add_foreign_key "notifications", "orders"
+  add_foreign_key "order_account_order_documents", "order_accounts"
+  add_foreign_key "order_account_order_documents", "order_documents"
   add_foreign_key "order_accounts", "accounts"
   add_foreign_key "order_accounts", "orders"
   add_foreign_key "order_documents", "documents"

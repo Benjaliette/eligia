be5778de0c1cc023ef0c88e4bc47503243a71be6
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-17 12:05:04 +0100

    add merci_facteurs controller + endpoint to MF webhook

--- app/controllers/merci_facteur_controller.rb
+++
@@ -1,3 +0,0 @@
-class MerciFacteurController < ApplicationController
-
-end
---
+++ app/controllers/merci_facteurs_controller.rb
@@ -0,0 +1,9 @@
+class MerciFacteursController < ApplicationController
+  skip_before_action :authenticate_user!, only: :webhook
+  skip_before_action :verify_authenticity_token, only: :webhook
+  skip_after_action :verify_authorized, only: :webhook
+
+  def webhook
+
+  end
+end

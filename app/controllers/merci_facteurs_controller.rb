a5403c0aed73956ecad926a8e488ce8d27ac24c4
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-17 18:16:14 +0100

    .......

--- app/controllers/merci_facteurs_controller.rb
+++ app/controllers/merci_facteurs_controller.rb
@@ -4,8 +4,8 @@ class MerciFacteursController < ApplicationController
   skip_after_action :verify_authorized, only: :webhook
 
   def webhook
-    webhook_event = JSON.parse(params["event"], sympbolize_names: true)
-    webhook_detail = JSON.parse(params["detail"], sympbolize_names: true).first
+    webhook_event = JSON.parse(params["event"], symbolize_names: true)
+    webhook_detail = JSON.parse(params["detail"], symbolize_names: true).first
 
     delivery = Delivery.find_by(envoi_id: webhook_detail[:id_envoi])
     delivery.update_state(webhook_event[:name_event])

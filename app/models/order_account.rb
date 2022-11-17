ba9a651ad296d3321d1215f5fa8d665c496e229c
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-17 14:52:10 +0100

    test method get + ip for request

--- app/models/order_account.rb
+++ app/models/order_account.rb
@@ -157,7 +157,6 @@ class OrderAccount < ApplicationRecord
 
     response = connection.post do |req|
       req.body = URI.encode_www_form(generate_json_send_resiliation(order_account)).gsub("%3A", "%22").gsub("%3D%3E", "%22%3A")
-
     end
     response = JSON.parse(response.body, symbolize_names: true)
   end

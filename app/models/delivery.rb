719d6f505c0fa70e089d9159b8d618f166107e7f
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-17 18:10:32 +0100

    .....

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,7 +2,28 @@ class Delivery < ApplicationRecord
   include AASM
   belongs_to :order_account
 
+  def update_state(event_name)
+    case event_name
+    when "new" then self.declare_new
+    when "printed" then self.declare_printed
+    when "sended" then self.declare_sended
+  end
+
   aasm do
     state :before_send, initial: true
+
+    state :new, :printed, :sended
+
+    event :declare_new do
+      transitions to: :new
+    end
+
+    event :declare_printed do
+      transitions to: :printed
+    end
+
+    event :declare_sended do
+      transitions to: :sended
+    end
   end
 end

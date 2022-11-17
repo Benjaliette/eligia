580d3948be25792afec634ca8da7b015a44b5641
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-17 10:15:21 +0100

    skip authenticate user for CGU and CGV pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home price contact]
+  skip_before_action :authenticate_user!, only: %i[home price cgu cgv]
 
   def home
   end
@@ -8,6 +8,9 @@ class PagesController < ApplicationController
     @packs = Pack.all.last(3)
   end
 
-  def contact
+  def cgu
+  end
+
+  def cgv
   end
 end

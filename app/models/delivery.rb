5a9899277aa2a096f88cc785f4825fea7d3a0296
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-18 12:01:46 +0100

    add some delivery aasm states + deal with webhook_details array

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -7,13 +7,15 @@ class Delivery < ApplicationRecord
     when "new" then self.declare_new!
     when "printed" then self.declare_printed!
     when "sended" then self.declare_sended!
+    when "delivered" then self.declare_delivered!
+    when "error" then self.declare_error!
     end
   end
 
   aasm do
     state :before_send, initial: true
 
-    state :new, :printed, :sended
+    state :new, :printed, :sended, :delivered, :error, :avis_reception_elec, :preuve_depot_dispo
 
     event :declare_new do
       transitions to: :new
@@ -25,6 +27,22 @@ class Delivery < ApplicationRecord
 
     event :declare_sended do
       transitions to: :sended
+    end
+
+    event :declare_delivered do
+      transitions to: :delivered
+    end
+
+    event :declare_error do
+      transitions to: :error
+    end
+
+    event :declare_avis_reception_elec do
+      transitions to: :avis_reception_elec
+    end
+
+    event :declare_preuve_depot_dispo do
+      transitions to: :preuve_depot_dispo
     end
   end
 end

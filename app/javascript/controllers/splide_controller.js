dbd4fab5dd3cc4613b8df9cd9ea08c4b55f993cc
Marc Delesalle <mrc.delesalle@gmail.com>
2022-11-25 16:06:11 +0100

    carousel responsive

--- app/javascript/controllers/splide_controller.js
+++ app/javascript/controllers/splide_controller.js
@@ -4,12 +4,24 @@ import Splide from '@splidejs/splide'
 // Connects to data-controller="splide"
 export default class extends Controller {
   connect() {
-    new Splide( '.splide',{
+    new Splide( '.splide', {
       type   : 'loop',
       speed: 800,
       padding: '20%',
       fixedWidth : '60%',
-      gap : '1em'
-    } ).mount();
+      gap : '1em',
+      breakpoints: {
+        900: {
+          type   : 'slide',
+          fixedWidth : '80%',
+          padding: {left:0, right: '10%'},
+        },
+        600: {
+          fixedWidth : '90%',
+          padding: {left:0, right: '5%'},
+        },
+      }
+    }
+     ).mount();
   }
 }

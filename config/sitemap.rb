213e951856ae6dd68bb333da01c54ad79955a549
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-12-19 12:17:06 +0100

    generated a sitemap.xml.gz with a sitemap.rb file

---
+++ config/sitemap.rb
@@ -0,0 +1,41 @@
+require 'google/cloud/storage'
+
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.eligia.fr/"
+SitemapGenerator::Sitemap.public_path = 'public'
+
+# SitemapGenerator::Sitemap.sitemaps_host = "https://storage.googleapis.com/eligia_dev/"
+# SitemapGenerator::Sitemap.sitemaps_path = 'sitemap/'
+# SitemapGenerator::Sitemap.adapter = SitemapGenerator::GoogleStorageAdapter.new(
+#   acl: 'public', # Optional.  This is the default value.
+#   bucket: 'eligia_dev',
+#   credentials: ENV['GOOGLE_APPLICATION_CREDENTIALS'].as_json
+# )
+
+SitemapGenerator::Sitemap.create do
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+
+  add price_pages_path
+  add cgu_pages_path
+  add cgs_pages_path
+  add contrats_helps_path
+  add documents_helps_path
+  add recapitulatif_helps_path
+  add tarifs_helps_path
+  add helps_path
+  add new_message_path
+  add new_user_session_path
+  add new_user_registration_path
+
+  # Add '/blog'
+  add blogposts_path, changefreq: 'daily'
+  #
+  # Add all articles:
+  Blogpost.find_each do |article|
+    add blogpost_path(article), lastmod: article.updated_at
+  end
+end
